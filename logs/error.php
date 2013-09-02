645c19a2e9284e2643d04ea4e3c982226699984d
sara nasereddin <anzeigen@runze-casper.de>
2013-09-02 19:13:54 +0200

    first commit

---
+++ logs/error.php
@@ -0,0 +1,20 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-07-30 12:50:21 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-07-30T12:50:21+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T12:51:05+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T12:51:21+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T12:57:37+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T12:57:39+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T12:57:52+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T14:41:25+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-07-30T15:07:19+00:00	INFO	joomlafailure	Ein leeres Passwort ist nicht zulässig
+2013-08-26T17:16:17+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-08-26T17:16:28+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-08-27T10:10:38+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-08-27T10:10:47+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-08-27T14:58:13+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
+2013-09-02T13:38:03+00:00	INFO	joomlafailure	Benutzername und Passwort falsch oder das Benutzerkonto existiert noch nicht!
