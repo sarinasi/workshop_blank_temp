9c62614f11281cc1c4af02e03eed2ccd619b371d
sara nasereddin <anzeigen@runze-casper.de>
2013-09-16 20:04:29 +0200

    Navbar und Menüs angelegt

--- templates/sarastemplate/index.php
+++ templates/sarastemplate/index.php
@@ -30,8 +30,8 @@ if ($modernizr==1) $doc->addScript($tpath.'/js/modernizr-2.6.2.js'); // <- this 
 unset($doc->_scripts[$this->baseurl.'/media/system/js/mootools-core.js']);
 unset($doc->_scripts[$this->baseurl.'/media/system/js/core.js']);
 unset($doc->_scripts[$this->baseurl.'/media/system/js/caption.js']);
-
-?><!doctype html>
+?>
+<!doctype html>
 <!--[if IEMobile]><html class="iemobile" lang="<?php echo $this->language; ?>"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="<?php echo $this->language; ?>"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="<?php echo $this->language; ?>"> <![endif]-->
@@ -44,6 +44,8 @@ unset($doc->_scripts[$this->baseurl.'/media/system/js/caption.js']);
   <link rel="apple-touch-icon-precomposed" href="<?php echo $tpath; ?>/apple-touch-icon-57x57.png"> <!-- iphone, ipod, android -->
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $tpath; ?>/apple-touch-icon-72x72.png"> <!-- ipad -->
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $tpath; ?>/apple-touch-icon-114x114.png"> <!-- iphone retina -->
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/bootstrap.css" type="text/css" />
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/sarastemplate/css/template.css" type="text/css" />
   <?php if ($pie==1) : ?>
     <!--[if lte IE 8]>
       <style> 
@@ -51,13 +53,37 @@ unset($doc->_scripts[$this->baseurl.'/media/system/js/caption.js']);
       </style>
     <![endif]-->
   <?php endif; ?>
+ 
 </head>
-	
-<body class="<?php echo $pageclass; ?>">
-  <!-- 
-    YOUR CODE HERE
-  -->
-  <jdoc:include type="modules" name="debug" />
+  
+<body class="page_bg">
+
+ 
+  
+    <div class="container"><div class="row">
+    <div class="navbar span12"><div class="navbar-inner">
+    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+    <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a> 
+   <a class="brand" href="<?php echo $this->baseurl; ?>/"><?php echo $app->getCfg('sitename'); ?></a>
+   <div class="nav-collapse row"><jdoc:include type="modules" name="menu" /><jdoc:include type="modules" name="search" /> 
+   </div>
+   </div>
+   </div>
+   </div>
+   </div>
+    <jdoc:include type="modules" name="debug" />
+  <script type="text/javascript">
+ (function($){
+	 $(document).ready(function(){
+		// dropdown
+		$('.menu > .parent').addClass('dropdown');
+		$('.menu > .parent > a').addClass('dropdown-toggle');
+		$('.menu > .parent > a').attr('data-toggle', 'dropdown');
+		$('.menu > .parent > a').append('<b class="caret"></b>');
+		$('.menu > .parent > ul').addClass('dropdown-menu');
+	 });
+ })(jQuery);
+ </script>
 </body>
 
 </html>
